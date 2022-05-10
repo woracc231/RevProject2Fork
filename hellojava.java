8ab923b61b416cb81692b4b93d50260187f1b854
Jason Maggart <jmaggart1@gmail.com>
2022-05-09 20:23:07 -0400

    Challenge1

---
+++ hellojava.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class hellojava{
+
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+String stuff = input.nextLine();
+
+System.out.println(stuff);
+}
+}
